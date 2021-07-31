e190de9fac9c32e6936b657953c5e6e3cf9a01b8
Deepen Dhulla <deepen@deependhulla.com>
2021-07-31 15:50:45 +0530

    update 2021-07-31

--- 03-upate-spamassassin.sh
+++ 03-upate-spamassassin.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+echo "updating ..pelase wait"
+## more useful for ONlY MX
+#extra-spamassassin-data/FromNotReplyTo.cf
+#extra-spamassassin-data/FromNotReplyToSameDomain.cf
+#extra-spamassassin-data/MultipleFromAT-rules.cf
+#extra-spamassassin-data/MXDomainCheckFromTo-rules.cf
+## useful for filtering in dovecot or outlook based on custom flag
+## X-Spam-Promotions: YES
+#extra-spamassassin-data/promotions-mail-tagging.cf
+
 /bin/cp -pR extra-spamassassin-data/* /etc/mail/spamassassin/
 
 cd ti-spam-filter
 ./makecf-ti-spam-content.sh
 cd -
+echo "Done";
